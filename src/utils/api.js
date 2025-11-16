d6bf87294fa8aa6f0f760b54c07d7af6ecbf9685
I.Y.G Nagendra <103441226+yogiganesh108@users.noreply.github.com>
2025-11-16 15:21:36 +0530

    Change API base URL from 8083 to 8081

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API = {
-  base: 'http://localhost:8083',
+  base: 'http://localhost:8081',
   async login(creds){
     const res = await fetch(`${this.base}/api/auth/login`, {
       method: 'POST',
